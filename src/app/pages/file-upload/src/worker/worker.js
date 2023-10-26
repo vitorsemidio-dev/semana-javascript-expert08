d68599198c3f1fc0c49eae21e0fb3b95c274465c
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-26 18:58:51 -0300

    feat: Adicionado configurações dos vídeos ao worker

--- src/app/pages/file-upload/src/worker/worker.js
+++ src/app/pages/file-upload/src/worker/worker.js
@@ -1,11 +1,47 @@
-onmessage = ({ data }) => {
+// import VideoProcessor from './video-processor.js';
+
+// const videoProcessor = new VideoProcessor();
+
+const qvgaContaints = {
+  width: 320,
+  height: 240,
+};
+
+const vgaContaints = {
+  width: 640,
+  height: 480,
+};
+
+const hdContains = {
+  width: 1280,
+  height: 720,
+};
+
+const encoderConfig = {
+  ...qvgaContaints,
+  bitrate: 10e6,
+  // ? WebM container format
+  codec: 'vp09.00.10.08',
+  pt: 4,
+  hardwareAcceleration: 'prefer-software',
+  // ? MP4
+  // codec: 'avc1.42002A',
+  // pt: 1,
+  // hardwareAcceleration: 'prefer-hardware',
+  // avc: { format: 'annexb' },
+};
+
+onmessage = async ({ data }) => {
   console.log('Iniciando Worker', data);
 
-  setTimeout(() => {
-    self.postMessage({
-      status: 'done',
-    });
-  }, 2000);
+  // await videoProcessor.start({
+  //   file: data.file,
+  //   encoderConfig,
+  // });
+
+  self.postMessage({
+    status: 'done',
+  });
 
   // troll
   // while (true) {}
