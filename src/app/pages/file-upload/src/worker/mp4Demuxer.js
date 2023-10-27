4d386ac99f7b322375190d903107464b85987ae9
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-27 07:08:34 -0300

    feat: extração dados do vídeo

--- src/app/pages/file-upload/src/worker/mp4Demuxer.js
+++ src/app/pages/file-upload/src/worker/mp4Demuxer.js
@@ -1,4 +1,4 @@
-import { createFile } from '../deps/mp4box.0.5.2.js';
+import { createFile, DataStream } from '../deps/mp4box.0.5.2.js';
 
 export default class MP4Demuxer {
   #onConfig;
@@ -19,15 +19,54 @@ export default class MP4Demuxer {
 
     this.#file = createFile();
 
-    this.#file.onReach = (args) => {
-      console.log(args);
-    };
+    this.#file.onReady = this.#onReady.bind(this);
 
-    this.#file.onError = (error) => {
-      console.error(error);
-    };
+    this.#file.onSamples = this.#onSamples.bind(this);
+
+    this.#file.onError = (error) => console.error('deu ruim mp4Demuxer', error);
 
     return this.#init(stream);
+  }
+
+  #description({ id }) {
+    const track = this.#file.getTrackById(id);
+    for (const entry of track.mdia.minf.stbl.stsd.entries) {
+      const box = entry.avcC || entry.hvcC || entry.vpcC || entry.av1C;
+      if (box) {
+        const stream = new DataStream(undefined, 0, DataStream.BIG_ENDIAN);
+        box.write(stream);
+        return new Uint8Array(stream.buffer, 8); // Remove the box header.
+      }
+    }
+    throw new Error('avcC, hvcC, vpcC, or av1C box not found');
+  }
+
+  #onReady(info) {
+    const [track] = info.videoTracks;
+    this.#onConfig({
+      codec: track.codec,
+      codedHeight: track.video.height,
+      codedWidth: track.video.width,
+      description: this.#description(track),
+      durationSecs: info.duration / info.timescale,
+    });
+
+    this.#file.setExtractionOptions(track.id);
+    this.#file.start();
+  }
+
+  #onSamples(trackId, ref, samples) {
+    // Generate and emit an EncodedVideoChunk for each demuxed sample.
+    for (const sample of samples) {
+      this.#onChunk(
+        new EncodedVideoChunk({
+          type: sample.is_sync ? 'key' : 'delta',
+          timestamp: (1e6 * sample.cts) / sample.timescale,
+          duration: (1e6 * sample.duration) / sample.timescale,
+          data: sample.data,
+        }),
+      );
+    }
   }
 
   /**
@@ -35,13 +74,21 @@ export default class MP4Demuxer {
    * @param {ReadableStream} stream
    */
   #init(stream) {
+    let _offset = 0;
     const consumeFile = new WritableStream({
       /**
        *
        * @param {Uint8Array} chunk
        */
-      write: (chunk) => {},
-      close: () => {},
+      write: (chunk) => {
+        const copy = chunk.buffer;
+        copy.fileStart = _offset;
+        this.#file.appendBuffer(copy);
+        _offset += chunk.length;
+      },
+      close: () => {
+        this.#file.flush();
+      },
     });
 
     return stream.pipeTo(consumeFile);
